643ac8247580c3202858683df709ac4ab86d478d
carlynorama <carlynorama@gmail.com>
2022-09-20 12:24:08 -0700

    Initial Commit

---
+++ LocationExplorer/LocationExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationExplorerApp.swift
+//  LocationExplorer
+//
+//  Created by Labtanza on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct LocationExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
